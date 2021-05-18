cf44c9d6ef28237483b131bfc969759c7f4808d6
Hanzala-FA18-bse-037 <FA18-BSE-037@cuilahore.edu.pk>
2021-05-18 11:01:06 +0500

    comit 4

--- FA18-BSE-037.js
+++ FA18-BSE-037.js
@@ -1,3 +1,54 @@
-fetch('https://jsonplaceholder.typicode.com/')
-  .then((response) => response.text())
-  .then((json) => console.log(json));
+window.currentRowID = 0;
+window.updateID = undefined;
+$(document).ready(function () {
+    $("#personUpdateForm").submit(function (event) {
+        event.preventDefault();
+        var email = $("input[name='email']").val();
+        var id = $("input[name='userId']").val();
+        $.ajax({
+            url: "https://jsonplaceholder.typicode.com/users/"+id,
+            type: "PUT",
+            beforeSend: function () {
+                $("#updateBtn").text("Updating").attr("disable", "disabled");
+            }
+        }).done(function (response) {
+            event.target.reset();
+            $("#" + id+ " .email").text(email);
+            $("#updateFormDiv").hide();
+        }).catch(function (error) {
+            $("#updateBtn").text("Update").removeAttr("disable");
+            console.log(error);
+            alert("An Error Occured while Updating Email. Please see Console for Error Details.");
+        });
+    });
+    $("#cancelBtnBtn").click(function () {
+        $("#updateFormDiv").hide();
+    })
+    $("#list tbody").on("click", ".update", function (e) {
+        window.updateID = $(e.currentTarget.closest('tr')).attr("id");
+        $("input[name='email']").val($("#" + window.updateID + " .email").text());
+        $("input[name='userId']").val(window.updateID);
+        $("#updateFormDiv").show();
+    });
+    $.ajax({
+        url: "https://jsonplaceholder.typicode.com/users",
+        type: "GET"
+    }).done(function (response) {
+        console.log(response);
+        response.map(function (value) {
+            $("#noRecordRow").remove();
+            $("#list tbody").append(function () {
+                return "<tr id=\"" + value.id + "\">" +
+                    "<td class=\"name\">" + value.name + "</td>" +
+                    "<td class=\"email\">" + value.email + "</td>" +
+                    "<td>" +
+                    "<a href=\"./view-albums.html?user="+value.id+"\"><button type=\"button\">Album</button></a>" +"   "+
+                    "<button class=\"update\">Update Email</button>" +
+                    "</td></tr>";
+            });
+        })
+    }).catch(function (error) {
+        console.log(error);
+        $("#noRecordRow td").text("Unable to fetch Record. Please see Console for Errors");
+    })
+});
